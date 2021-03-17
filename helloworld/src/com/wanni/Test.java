0b8a238fa5b4ee3018b8a700a8034b72c267508f
wannihedi <hedi.wanni@gmail.com>
2021-03-17 16:12:32 +0100

    code java et somme de deux variable

---
+++ helloworld/src/com/wanni/Test.java
@@ -0,0 +1,20 @@
+package com.wanni;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=5;
+		int b=5;
+		int z=a+b;
+		int i ;
+		for (i=0 ;i<5 ;i++) {
+			
+			System.out.println("bienvenu eclips ");
+		}
+		System.out.println("somme de a +b " + z);
+		System.out.println("bonjour wanni");
+	}
+
+}
